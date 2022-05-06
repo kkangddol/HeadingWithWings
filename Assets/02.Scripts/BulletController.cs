57fd2761bfeae9911112273ffa05210b05572422
SeokWon Kang <knemo333@gmail.com>
2022-05-06 22:59:40 +0900

    총알 기존 발사방식대로 EnemyPool, BulletPool 제작

--- Assets/02.Scripts/BulletController.cs
+++ Assets/02.Scripts/BulletController.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    [SerializeField]
-    private PlayerInfo player;
     public int damage;    //총알공격력  //생성할때 ShootingController에서 넘겨받음
     public float speed = 500.0f;
     public int knockbackSize = 5;
     private Rigidbody rb;
 
-    void Start()
+    void Awake()
     {
         //player = GameObject.FindWithTag("PLAYER").GetComponent<PlayerInfo>();
         //damage = player.damage;
         rb = GetComponent<Rigidbody>();
         //rb.AddRelativeForce(Vector3.forward * speed); //일반 instantiate 및 instantiateComponent
-        rb.AddRelativeForce(transform.forward * speed); //instantiateGeneric
-        Destroy(gameObject, 10.0f);
+        //rb.AddRelativeForce(transform.forward * speed); //instantiateGeneric
+        //Destroy(gameObject, 10.0f);
+    }
+
+    public void ActivateBullet()
+    {
+        rb.AddRelativeForce(transform.forward * speed);
+        Invoke("DestroyBullet", 10.0f);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -26,8 +30,16 @@ public class BulletController : MonoBehaviour
         if(other.tag == "ENEMY")
         {
             other.GetComponent<EnemyPrototype>().TakeDamage(transform, damage, knockbackSize);
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            DestroyBullet();
         }
+    }
+
+    private void DestroyBullet()
+    {
+        
+        BulletPool.ReturnObject(this);
+
     }
 
     //사용하지않는 update함수는 지워야 최적화된다
--- Assets/02.Scripts/ShootingController.cs
+++ Assets/02.Scripts/ShootingController.cs
@@ -11,7 +11,7 @@ public class ShootingController : MonoBehaviour
     private bool FireState = true;
 
 
-    public GameObject bullet;   //Prefabs을 GameObject로 받는다 아니면 transformComponent로 받아도 된다
+    //public GameObject bullet;   //Prefabs을 GameObject로 받는다 아니면 transformComponent로 받아도 된다
     public BulletController bulletController;
     public Transform firePos;
     public AudioClip fireSfx;
@@ -65,9 +65,16 @@ public class ShootingController : MonoBehaviour
 
 
         //총알 생성 제너릭 방식 연습
-        BulletController firedBullet = Instantiate<BulletController>(bulletController);
+        //BulletController firedBullet = Instantiate<BulletController>(bulletController);
+        //firedBullet.damage = playerInfo.damage;
+        //firedBullet.transform.SetPositionAndRotation(firePos.position,firePos.rotation);
+
+        //오브젝트 풀 방식
+        BulletController firedBullet = BulletPool.GetObject();
+        firedBullet.transform.SetPositionAndRotation(firePos.position, firePos.rotation);
         firedBullet.damage = playerInfo.damage;
-        firedBullet.transform.SetPositionAndRotation(firePos.position,firePos.rotation);
+        firedBullet.ActivateBullet();
+        
 
         //총 소리 발생
         
