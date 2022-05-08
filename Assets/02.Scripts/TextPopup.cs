33c11d0a6bec70dcaa674e4e3840ceef483aaa4c
SeokWon Kang <knemo333@gmail.com>
2022-05-08 15:23:01 +0900

    체력,산소,데미지 텍스트 통합
    TextPopup 프리팹으로 통합, 코드도 TextPopup.cs 로 통합.
    앞으로 체력관련 텍스트 생서할때 Set~~ 이용해서 값만 넣어주면 색상은 알아서 들어감

---
+++ Assets/02.Scripts/TextPopup.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextPopup : MonoBehaviour
+{
+    private Camera mainCamera;
+    TextMeshPro textMeshPro;
+    private int textValue;
+    private Color color;
+
+    public void  SetDamage(int damage)
+    {
+        textValue = damage;
+        color = Color.red;
+    }
+    public void  SetHealAmount(int healAmount)
+    {
+        textValue = healAmount;
+        color = Color.green;
+    }
+    public void SetOxygenAmount(int oxygenAmount)
+    {
+        textValue = oxygenAmount;
+        color = Color.cyan;
+    }
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        transform.LookAt(transform.position + mainCamera.transform.forward);
+        textMeshPro = GetComponent<TextMeshPro>();
+        textMeshPro.text = textValue.ToString();
+        textMeshPro.color = color;
+        Destroy(gameObject, 1.15f);
+    }
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + mainCamera.transform.forward);
+    }
+}
