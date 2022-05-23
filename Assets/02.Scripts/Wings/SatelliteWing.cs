ed21c5d0252d63f5603798a0584f36cdf764b3a5
SeokWon Kang <knemo333@gmail.com>
2022-05-23 18:07:19 +0900

    세이렌 날개 틀 제작
    게임에서 날개 변경하는 부분 제거하고 그냥 누적해서 아이템을 얻고 적용하는 식으로 바꾸는게 좋을 것 같음

--- Assets/02.Scripts/Wings/SatelliteWing.cs
+++ Assets/02.Scripts/Wings/SatelliteWing.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SatelliteWing : MonoBehaviour
 {
     private PlayerInfo playerInfo;
+    private GameObject playerController;
     public Satellite satellitePrefab;
     private Satellite[] satellites;
     public int damage;
@@ -22,19 +23,20 @@ public class SatelliteWing : MonoBehaviour
     {
         playerInfo = GetComponent<PlayerInfo>();
         satellites = new Satellite[2];
+        playerController = GameObject.FindGameObjectWithTag("PLAYER");
         SummonSatellite();
     }
 
     void SummonSatellite()
     {
         satellites[0] = Instantiate(satellitePrefab, transform.position + (transform.forward * orbit), Quaternion.identity);
-        satellites[0].transform.SetParent(transform);
+        satellites[0].transform.SetParent(playerController.transform);
         satellites[0].damage = this.damage;
         satellites[0].knockbackSize = this.knockbackSize;
         satellites[0].orbitSpeed = this.orbitSpeed;
         
         satellites[1] = Instantiate(satellitePrefab, transform.position - (transform.forward * orbit), Quaternion.identity);
-        satellites[1].transform.SetParent(transform);
+        satellites[1].transform.SetParent(playerController.transform);
         satellites[1].damage = this.damage;
         satellites[1].knockbackSize = this.knockbackSize;
         satellites[1].orbitSpeed = this.orbitSpeed;
