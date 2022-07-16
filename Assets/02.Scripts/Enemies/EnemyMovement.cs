d76de054a2d0e711e6fbb59bc3ed81fc0d30fc4d
SeokWon Kang <knemo333@gmail.com>
2022-07-17 03:49:34 +0900

    캐릭터 이동, 적 이동 물리 추가
    캐릭터 이동과 적 이동에 물리 추가 완료.
    
    적 이동을 2가지로 나눔
    
    고정적으로 나아가는 움직임과
    
    주기적으로 미끄러지며 나아가는 움직임
    
    두 움직임 모두 StopMove와 ResumeMove가 필요하고 또 외부에서 호출할 필요가 있으므로 인터페이스로 분리해둠

--- Assets/02.Scripts/Enemies/EnemyMovement.cs
+++ Assets/02.Scripts/Enemies/EnemyMovement.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour, IEnemyStopHandler
 {
     private EnemyInfo enemyInfo;
     private SpriteRenderer enemySprite;
-    public bool isStop = false;
     private float enemySpeed = 0.0f;
     private Color freezeColor = new Color(0f, 1f, 1f, 0.95f);
+    bool isStop = false;
+    public bool IsStop
+    {
+        get{ return isStop;}
+        set{ isStop = value;}
+    }
+    Vector2 currentPos;
+    Rigidbody2D rigid;
+    Vector2 moveDirection;
+
+
 
     private void Start()
     {
@@ -20,30 +30,38 @@ public class EnemyMovement : MonoBehaviour
         enemyInfo = GetComponent<EnemyInfo>();
         enemySprite = GetComponentInChildren<SpriteRenderer>();
         enemySpeed = enemyInfo.enemyMoveSpeed;
-    }
-
-    private void Update() {
-        if(!isStop)
-        {
-            FollowTarget();
-        }
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
-        if(transform.position.x - GameManager.playerTransform.position.x > 0)
-            GetComponentInChildren<SpriteRenderer>().flipX = false;
-        else if(transform.position.x - GameManager.playerTransform.position.x < 0)
-            GetComponentInChildren<SpriteRenderer>().flipX = true;
+        if(!isStop)
+        {
+            MoveToTarget();
+        }
     }
 
-    void FollowTarget()
+    // void FollowTarget()
+    // {
+    //     //transform.position = Vector2.MoveTowards(transform.position, GameManager.playerTransform.position, enemyInfo.enemyMoveSpeed * Time.deltaTime);
+    //     currentPos = Vector2.MoveTowards(transform.position, GameManager.playerTransform.position, Time.fixedDeltaTime * enemyInfo.enemyMoveSpeed);
+    //     rigid.MovePosition(new Vector2(currentPos.x, currentPos.y));
+    // }
+
+    void Tracking()
     {
-        transform.position = Vector2.MoveTowards(transform.position, GameManager.playerTransform.position, enemyInfo.enemyMoveSpeed * Time.deltaTime);
+        moveDirection = (GameManager.playerRigidbody.position - rigid.position).normalized;
+    }
+
+    void MoveToTarget()
+    {
+        Tracking();
+        rigid.AddForce(moveDirection * enemyInfo.enemyMoveSpeed);
     }
 
     public void StopMove()
     {
         isStop = true;
+        rigid.velocity = Vector2.zero;
     }
     public void ResumeMove()
     {
