f951d4b96c90a0fca1912006ba97a1333eb2fc19
IndiaInk10 <indiaink10@gmail.com>
2022-07-19 16:36:45 +0900

    악마 날개 장비 기능 구현
    악마 날개 장비 기능 구현과 기존 코드들의 약간의 코드 수정(나누기 연산 -> 곱 연산) 해주었습니다.
    몇몇 파일들의 이름을 변경해주었습니다.

---
+++ Assets/02.Scripts/Equipments/WingEquipments/DevilWing.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevilWing : Equipment, ActiveWing
+{
+    PlayerInfo playerInfo;
+    public GameObject[] skillObjects;
+    public float damageMultiplier;
+    public float skillDelayMultiplier;
+    public float knockbackSize;
+    public float skillTime;
+    private bool isCoolDown = false;
+    public float coolTime;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        playerInfo = GameManager.playerInfo;
+    }
+
+    public void ActivateSkill()
+    {
+        //집중 포화
+        if (isCoolDown) return;
+
+        StartCoroutine(ClawAttack());
+    }
+
+    IEnumerator ClawAttack()
+    {
+        isCoolDown = true;
+
+        foreach(GameObject skillObject in skillObjects)
+        {
+            skillObject.SetActive(true);
+            SetSkillInfo(skillObject);
+
+            yield return new WaitForSeconds(0.3f);
+
+            // skillTime 및 StopSkill 고려
+            skillObject.SetActive(false);
+            skillObject.transform.localRotation = Quaternion.AngleAxis(10, Vector3.forward);
+        }
+
+        StartCoroutine(CoolDown());
+    }
+    private void SetSkillInfo(GameObject go)
+    {
+        DevilWingSkill temp = go.GetComponent<DevilWingSkill>();
+        temp.damage = playerInfo.damage * damageMultiplier;
+        temp.knockbackSize = knockbackSize;
+        temp.Init();
+    }
+
+    private void SetActiveFalseAll()
+    {
+        foreach(GameObject skillObject in skillObjects)
+        {
+            skillObject.SetActive(false);
+        }
+    }
+
+    IEnumerator CoolDown()
+    {
+        isCoolDown = true;
+        coolTime = playerInfo.skillDelay * skillDelayMultiplier;
+        while (isCoolDown)
+        {
+            yield return null;
+            coolTime -= Time.deltaTime;
+            if (coolTime <= 0)
+            {
+                isCoolDown = false;
+                break;
+            }
+        }
+    }
+
+
+    public override void SetLevel(int newLevel)
+    {
+        this.level = newLevel;
+
+        //220527 하드코딩이므로 DataManager 이용할 것.
+        switch (level)
+        {
+            case 1:
+                {
+                    break;
+                }
+            case 2:
+                {
+                    break;
+                }
+            case 3:
+                {
+                    break;
+                }
+            case 4:
+                {
+                    break;
+                }
+            case 5:
+                {
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+}
