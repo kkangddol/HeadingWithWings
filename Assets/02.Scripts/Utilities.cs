bd0d9d41bf9b4f27fc60884829646b3704b22e6c
IndiaInk10 <indiaink10@gmail.com>
2022-07-22 19:22:21 +0900

    LookAt2 버전 추가
    Transform from, Vector3 to 버전 추가 완료

--- Assets/02.Scripts/Utilities.cs
+++ Assets/02.Scripts/Utilities.cs
@@ -9,9 +9,18 @@ public static class Utilities
         float angle = GetAngle(from, to);
         return Quaternion.AngleAxis(angle, Vector3.forward);
     }
+    public static Quaternion LookAt2(Transform from, Vector3 to)
+    {
+        float angle = GetAngle(from, to);
+        return Quaternion.AngleAxis(angle, Vector3.forward);
+    }
 
     public static float GetAngle(Transform from, Transform to)
     {
         return Mathf.Atan2(to.position.y - from.position.y, to.position.x - from.position.x) * Mathf.Rad2Deg;
+    }
+    public static float GetAngle(Transform from, Vector3 to)
+    {
+        return Mathf.Atan2(to.y - from.position.y, to.x - from.position.x) * Mathf.Rad2Deg;
     }
 }
