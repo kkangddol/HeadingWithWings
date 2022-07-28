925bf11b8b92058b4188eefc939d3d0146d49c5e
IndiaInk10 <indiaink10@gmail.com>
2022-07-28 14:57:01 +0900

    큐피트 작업 완료

--- Assets/03.Enemy/Scripts/EnemyAttackParabola.cs
+++ Assets/03.Enemy/Scripts/EnemyAttackParabola.cs
@@ -4,36 +4,92 @@ using UnityEngine;
 
 public class EnemyAttackParabola : MonoBehaviour
 {
-    public GameObject bullet = null;
-    public Transform playerTr = null;
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    private Transform playerTransform;
+    private EnemyInfo enemyInfo;
+    private IEnemyStopHandler stopHandler;
+    public EnemyProjectile enemyProjectile;
+    public int projectileCount = 1;
+    private bool isAttacking;
+    private bool isInRange = false;
+    public bool IsInRange
     {
-        StartCoroutine(Test());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    IEnumerator Test()
-    {
-        while(true)
+        get
         {
-            yield return new WaitForSeconds(2f);
-
-            Vector3 toPlayerV3 = playerTr.position - this.transform.position;
-            toPlayerV3.x *= 0.5f;
-            toPlayerV3.y *= 2.0f;
-
-            GameObject go = Instantiate(bullet, this.transform.position, Utilities.LookAt2(this.transform, toPlayerV3));
-            go.GetComponent<Rigidbody2D>().AddForce(go.transform.right * toPlayerV3.magnitude, ForceMode2D.Impulse);
-            go.GetComponent<Rigidbody2D>().AddTorque(1f);
+            return isInRange;
+        }
+        set
+        {
+            isInRange = value;
         }
     }
+
+    private void Start()
+    {
+        Initialize();
+        StartCoroutine(CheckRange());
+    }
+
+    private void Update()
+    {
+        if (IsInRange)
+        {
+            stopHandler.StopMove();
+
+            if (isAttacking) return;
+
+            isAttacking = true;
+            Fire();
+            StartCoroutine(FireDelay());
+        }
+        else if (!IsInRange && stopHandler.IsStop)
+        {
+            stopHandler.ResumeMove();
+        }
+    }
+
+    void Initialize()
+    {
+        enemyInfo = GetComponent<EnemyInfo>();
+        playerTransform = enemyInfo.playerTransform;
+        stopHandler = GetComponent<IEnemyStopHandler>();
+        //attackRange = GameManager.Data.MonsterDict[enemyInfo.monsterID];
+        //fireDelay = GameManager.Data.MonsterDict[int.Parse(gameObject.name)].projectileFireDelay;
+        isInRange = false;
+        isAttacking = false;
+    }
+
+    IEnumerator CheckRange()
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(0.1f);
+        while (!enemyInfo.IsDead)
+        {
+            yield return waitTime;
+            if (Vector2.Distance(transform.position, playerTransform.position) <= enemyInfo.attackRange)
+            {
+                IsInRange = true;
+            }
+            else
+            {
+                IsInRange = false;
+            }
+        }
+    }
+
+    void Fire()
+    {
+        for (int i = 0; i < projectileCount; i++)
+        {
+            EnemyProjectile newProjectile = Instantiate<EnemyProjectile>(enemyProjectile, transform.position, Quaternion.identity);
+            newProjectile.damage = enemyInfo.projectileDamage;
+            ((EnemyParabolaProjectile)newProjectile).startPos = this.transform.position;
+            ((EnemyParabolaProjectile)newProjectile).targetPos = playerTransform.position;
+        }
+    }
+
+    IEnumerator FireDelay()
+    {
+        yield return new WaitForSeconds(enemyInfo.projectileFireDelay);
+        isAttacking = false;
+    }
+    
 }
